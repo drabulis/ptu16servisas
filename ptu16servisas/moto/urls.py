f45c0ba34623d7e37e75ddc68c460732bd615392
drabulis <naikintuvas111@gmail.com>
2023-10-04 14:49:37 +0300

    apsauginis commit

--- ptu16servisas/moto/urls.py
+++ ptu16servisas/moto/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('services/', views.services, name='services'),
+    path('models/', views.model, name='models'),
 ]
--- ptu16servisas/moto/views.py
+++ ptu16servisas/moto/views.py
@@ -5,7 +5,21 @@ def index(request):
     context = {
         'cars': models.CarModel.objects.all(),
         'customers': models.Customer.objects.all(),
-        'orders': models.OrderLine.objects.filter(status=3).count,
+        'orders': models.OrderLine.objects.filter(status=2).count,
         'services': models.PartService.objects.all(),
     }
     return render(request, 'library/index.html', context)
+
+def model(request):
+    return render(
+                request,
+                'library/models.html', 
+                {'models_list' :models.CarModel.objects.all()}
+                )
+
+def services(request):
+    return render(
+                request, 
+                'library/services.html', 
+                {'services_list' : models.PartService.objects.all()}
+                )
